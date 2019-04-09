d2630ca2b117ba2cf78397df1f21cd57bb1ccccf
houndtang <houndtang@etekcity.com.cn>
2019-04-09 09:14:31 +0800

    test

---
+++ server.php
@@ -0,0 +1,7 @@
+<?php
+$http = new Swoole\Http\Server("127.0.0.1", 9527);
+$http->on('request', function ($request, $response) {
+    var_dump($request->server);
+    $response->end("<h1>Hello Swoole.</h1>");
+});
+$http->start();
